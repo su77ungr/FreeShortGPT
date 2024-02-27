e85ce9a4039a24353228dbbbd1b7c8700ad1e600
stephen2nguyen <dulletavn@gmail.com>
2024-02-27 16:42:28 +0700

    ecom branch

--- gui/content_automation_ui.py
+++ gui/content_automation_ui.py
@@ -2,6 +2,7 @@ import gradio as gr
 
 from gui.ui_tab_short_automation import ShortAutomationUI
 from gui.ui_tab_video_automation import VideoAutomationUI
+from gui.ui_tab_product_video_automation import ProductVideoAutomationUI
 from gui.ui_tab_video_translation import VideoTranslationUI
 
 
@@ -16,9 +17,10 @@ class GradioContentAutomationUI:
             gr.Markdown("# 🏆 Content Automation 🚀")
             gr.Markdown("## Choose your desired automation task.")
             choice = gr.Radio(['🎬 Automate the creation of shorts', '🎞️ Automate a video with stock assets', '🌐 Automate multilingual video dubbing'], label="Choose an option")
+            product_video_automation_ui = ProductVideoAutomationUI(self.shortGPTUI).create_ui()
             video_automation_ui = VideoAutomationUI(self.shortGPTUI).create_ui()
             short_automation_ui = ShortAutomationUI(self.shortGPTUI).create_ui()
             video_translation_ui = VideoTranslationUI(self.shortGPTUI).create_ui()
             choice.change(lambda x: (gr.update(visible=x == choice.choices[1]), gr.update(visible=x == choice.choices[0]), gr.update(
-                visible=x == choice.choices[2])), [choice], [video_automation_ui, short_automation_ui, video_translation_ui])
+                visible=x == choice.choices[2]), gr.update(visible=x == choice.choices[3])), [choice], [video_automation_ui, short_automation_ui, video_translation_ui, product_video_automation_ui])
         return self.content_automation_ui
